34003172a2639bd5dc219af4281623f9af17b929
Lorenas <44069506+ulorenas@users.noreply.github.com>
2019-02-26 20:19:34 +0200

    yy

--- app/src/main/java/com/luta/semesterproject/MainActivity.java
+++ app/src/main/java/com/luta/semesterproject/MainActivity.java
@@ -13,5 +13,5 @@ public class MainActivity extends AppCompatActivity {
     // another test lol
     // test..\
     // yet another test
-    // is this another test? // sub //
+    // is this another test? // sub // // vienas
 }
